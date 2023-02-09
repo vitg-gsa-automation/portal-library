3d5c65e9c654700723d45540cedb32d5b83c3134
Aaron Nolan <aaron.nolan@volpegroup.com>
2023-02-09 17:41:29 -0600

    change libraryTarget to commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'umd',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
