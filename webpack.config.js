d2d71dff8f149f2affc251bd2e70f4854c8440e0
Aaron Nolan <aaron.nolan@volpegroup.com>
2023-02-09 15:37:31 -0600

    add to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'index.js',
     libraryTarget: 'umd',
   },
   module: {
