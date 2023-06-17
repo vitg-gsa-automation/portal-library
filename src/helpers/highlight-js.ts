9a5746a8f7cca25bff6df10fa7ae1142b4517926
Aaron Nolan <aaron.nolan@volpegroup.com>
2023-06-17 11:40:20 -0500

    try fix for languages

--- src/helpers/highlight-js.ts
+++ src/helpers/highlight-js.ts
@@ -1,5 +1,12 @@
 import 'highlight.js/styles/default.css';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
+import json from 'highlight.js/lib/languages/json.js';
+import xml from 'highlight.js/lib/languages/xml.js';
+import yaml from 'highlight.js/lib/languages/yaml.js';
+
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('yaml', yaml);
 
 export function highlightXML(xmlString: string): string {
   return hljs.highlight(xmlString, {
