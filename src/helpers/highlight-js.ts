6aa929ee52eeb0850368500d5371732589e9d00c
Aaron Nolan <aaron.nolan@volpegroup.com>
2023-06-17 21:32:01 -0500

    use custom languages instead. post issue on highlightjs repo

--- src/helpers/highlight-js.ts
+++ src/helpers/highlight-js.ts
@@ -1,10 +1,473 @@
 import 'highlight.js/styles/default.css';
 import hljs from 'highlight.js/lib/core';
-import json from 'highlight.js/lib/languages/json';
-import xml from 'highlight.js/lib/languages/xml';
-import yaml from 'highlight.js/lib/languages/yaml';
+// import json from 'highlight.js/lib/languages/json';
+// import xml from 'highlight.js/lib/languages/xml';
+// import yaml from 'highlight.js/lib/languages/yaml';
 
-console.log(json);
+function json(hljs: any) {
+  const ATTRIBUTE = {
+    className: 'attr',
+    begin: /"(\\.|[^\\"\r\n])*"(?=\s*:)/,
+    relevance: 1.01,
+  };
+  const PUNCTUATION = {
+    match: /[{}[\],:]/,
+    className: 'punctuation',
+    relevance: 0,
+  };
+  const LITERALS = ['true', 'false', 'null'];
+  // NOTE: normally we would rely on `keywords` for this but using a mode here allows us
+  // - to use the very tight `illegal: \S` rule later to flag any other character
+  // - as illegal indicating that despite looking like JSON we do not truly have
+  // - JSON and thus improve false-positively greatly since JSON will try and claim
+  // - all sorts of JSON looking stuff
+  const LITERALS_MODE = {
+    scope: 'literal',
+    beginKeywords: LITERALS.join(' '),
+  };
+
+  return {
+    name: 'JSON',
+    keywords: {
+      literal: LITERALS,
+    },
+    contains: [
+      ATTRIBUTE,
+      PUNCTUATION,
+      hljs.QUOTE_STRING_MODE,
+      LITERALS_MODE,
+      hljs.C_NUMBER_MODE,
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
+    ],
+    illegal: '\\S',
+  };
+}
+
+function xml(hljs: any) {
+  const regex = hljs.regex;
+  // XML names can have the following additional letters: https://www.w3.org/TR/xml/#NT-NameChar
+  // OTHER_NAME_CHARS = /[:\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]/;
+  // Element names start with NAME_START_CHAR followed by optional other Unicode letters, ASCII digits, hyphens, underscores, and periods
+  // const TAG_NAME_RE = regex.concat(/[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/, regex.optional(/[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]*:/), /[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]*/);;
+  // const XML_IDENT_RE = /[A-Z_a-z:\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]+/;
+  // const TAG_NAME_RE = regex.concat(/[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/, regex.optional(/[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]*:/), /[A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\u00B7\u0300-\u036F\u203F-\u2040]*/);
+  // however, to cater for performance and more Unicode support rely simply on the Unicode letter class
+  const TAG_NAME_RE = regex.concat(
+    /[\p{L}_]/u,
+    regex.optional(/[\p{L}0-9_.-]*:/u),
+    /[\p{L}0-9_.-]*/u
+  );
+  const XML_IDENT_RE = /[\p{L}0-9._:-]+/u;
+  const XML_ENTITIES = {
+    className: 'symbol',
+    begin: /&[a-z]+;|&#[0-9]+;|&#x[a-f0-9]+;/,
+  };
+  const XML_META_KEYWORDS = {
+    begin: /\s/,
+    contains: [
+      {
+        className: 'keyword',
+        begin: /#?[a-z_][a-z1-9_-]+/,
+        illegal: /\n/,
+      },
+    ],
+  };
+  const XML_META_PAR_KEYWORDS = hljs.inherit(XML_META_KEYWORDS, {
+    begin: /\(/,
+    end: /\)/,
+  });
+  const APOS_META_STRING_MODE = hljs.inherit(hljs.APOS_STRING_MODE, {
+    className: 'string',
+  });
+  const QUOTE_META_STRING_MODE = hljs.inherit(hljs.QUOTE_STRING_MODE, {
+    className: 'string',
+  });
+  const TAG_INTERNALS = {
+    endsWithParent: true,
+    illegal: /</,
+    relevance: 0,
+    contains: [
+      {
+        className: 'attr',
+        begin: XML_IDENT_RE,
+        relevance: 0,
+      },
+      {
+        begin: /=\s*/,
+        relevance: 0,
+        contains: [
+          {
+            className: 'string',
+            endsParent: true,
+            variants: [
+              {
+                begin: /"/,
+                end: /"/,
+                contains: [XML_ENTITIES],
+              },
+              {
+                begin: /'/,
+                end: /'/,
+                contains: [XML_ENTITIES],
+              },
+              { begin: /[^\s"'=<>`]+/ },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  return {
+    name: 'HTML, XML',
+    aliases: [
+      'html',
+      'xhtml',
+      'rss',
+      'atom',
+      'xjb',
+      'xsd',
+      'xsl',
+      'plist',
+      'wsf',
+      'svg',
+    ],
+    case_insensitive: true,
+    unicodeRegex: true,
+    contains: [
+      {
+        className: 'meta',
+        begin: /<![a-z]/,
+        end: />/,
+        relevance: 10,
+        contains: [
+          XML_META_KEYWORDS,
+          QUOTE_META_STRING_MODE,
+          APOS_META_STRING_MODE,
+          XML_META_PAR_KEYWORDS,
+          {
+            begin: /\[/,
+            end: /\]/,
+            contains: [
+              {
+                className: 'meta',
+                begin: /<![a-z]/,
+                end: />/,
+                contains: [
+                  XML_META_KEYWORDS,
+                  XML_META_PAR_KEYWORDS,
+                  QUOTE_META_STRING_MODE,
+                  APOS_META_STRING_MODE,
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      hljs.COMMENT(/<!--/, /-->/, { relevance: 10 }),
+      {
+        begin: /<!\[CDATA\[/,
+        end: /\]\]>/,
+        relevance: 10,
+      },
+      XML_ENTITIES,
+      // xml processing instructions
+      {
+        className: 'meta',
+        end: /\?>/,
+        variants: [
+          {
+            begin: /<\?xml/,
+            relevance: 10,
+            contains: [QUOTE_META_STRING_MODE],
+          },
+          {
+            begin: /<\?[a-z][a-z0-9]+/,
+          },
+        ],
+      },
+      {
+        className: 'tag',
+        /*
+        The lookahead pattern (?=...) ensures that 'begin' only matches
+        '<style' as a single word, followed by a whitespace or an
+        ending bracket.
+        */
+        begin: /<style(?=\s|>)/,
+        end: />/,
+        keywords: { name: 'style' },
+        contains: [TAG_INTERNALS],
+        starts: {
+          end: /<\/style>/,
+          returnEnd: true,
+          subLanguage: ['css', 'xml'],
+        },
+      },
+      {
+        className: 'tag',
+        // See the comment in the <style tag about the lookahead pattern
+        begin: /<script(?=\s|>)/,
+        end: />/,
+        keywords: { name: 'script' },
+        contains: [TAG_INTERNALS],
+        starts: {
+          end: /<\/script>/,
+          returnEnd: true,
+          subLanguage: ['javascript', 'handlebars', 'xml'],
+        },
+      },
+      // we need this for now for jSX
+      {
+        className: 'tag',
+        begin: /<>|<\/>/,
+      },
+      // open tag
+      {
+        className: 'tag',
+        begin: regex.concat(
+          /</,
+          regex.lookahead(
+            regex.concat(
+              TAG_NAME_RE,
+              // <tag/>
+              // <tag>
+              // <tag ...
+              regex.either(/\/>/, />/, /\s/)
+            )
+          )
+        ),
+        end: /\/?>/,
+        contains: [
+          {
+            className: 'name',
+            begin: TAG_NAME_RE,
+            relevance: 0,
+            starts: TAG_INTERNALS,
+          },
+        ],
+      },
+      // close tag
+      {
+        className: 'tag',
+        begin: regex.concat(
+          /<\//,
+          regex.lookahead(regex.concat(TAG_NAME_RE, />/))
+        ),
+        contains: [
+          {
+            className: 'name',
+            begin: TAG_NAME_RE,
+            relevance: 0,
+          },
+          {
+            begin: />/,
+            relevance: 0,
+            endsParent: true,
+          },
+        ],
+      },
+    ],
+  };
+}
+
+function yaml(hljs: any) {
+  const LITERALS = 'true false yes no null';
+
+  // YAML spec allows non-reserved URI characters in tags.
+  const URI_CHARACTERS = "[\\w#;/?:@&=+$,.~*'()[\\]]+";
+
+  // Define keys as starting with a word character
+  // ...containing word chars, spaces, colons, forward-slashes, hyphens and periods
+  // ...and ending with a colon followed immediately by a space, tab or newline.
+  // The YAML spec allows for much more than this, but this covers most use-cases.
+  const KEY = {
+    className: 'attr',
+    variants: [
+      { begin: '\\w[\\w :\\/.-]*:(?=[ \t]|$)' },
+      {
+        // double quoted keys
+        begin: '"\\w[\\w :\\/.-]*":(?=[ \t]|$)',
+      },
+      {
+        // single quoted keys
+        begin: "'\\w[\\w :\\/.-]*':(?=[ \t]|$)",
+      },
+    ],
+  };
+
+  const TEMPLATE_VARIABLES = {
+    className: 'template-variable',
+    variants: [
+      {
+        // jinja templates Ansible
+        begin: /\{\{/,
+        end: /\}\}/,
+      },
+      {
+        // Ruby i18n
+        begin: /%\{/,
+        end: /\}/,
+      },
+    ],
+  };
+  const STRING = {
+    className: 'string',
+    relevance: 0,
+    variants: [
+      {
+        begin: /'/,
+        end: /'/,
+      },
+      {
+        begin: /"/,
+        end: /"/,
+      },
+      { begin: /\S+/ },
+    ],
+    contains: [hljs.BACKSLASH_ESCAPE, TEMPLATE_VARIABLES],
+  };
+
+  // Strings inside of value containers (objects) can't contain braces,
+  // brackets, or commas
+  const CONTAINER_STRING = hljs.inherit(STRING, {
+    variants: [
+      {
+        begin: /'/,
+        end: /'/,
+      },
+      {
+        begin: /"/,
+        end: /"/,
+      },
+      { begin: /[^\s,{}[\]]+/ },
+    ],
+  });
+
+  const DATE_RE = '[0-9]{4}(-[0-9][0-9]){0,2}';
+  const TIME_RE = '([Tt \\t][0-9][0-9]?(:[0-9][0-9]){2})?';
+  const FRACTION_RE = '(\\.[0-9]*)?';
+  const ZONE_RE = '([ \\t])*(Z|[-+][0-9][0-9]?(:[0-9][0-9])?)?';
+  const TIMESTAMP = {
+    className: 'number',
+    begin: '\\b' + DATE_RE + TIME_RE + FRACTION_RE + ZONE_RE + '\\b',
+  };
+
+  const VALUE_CONTAINER = {
+    end: ',',
+    endsWithParent: true,
+    excludeEnd: true,
+    keywords: LITERALS,
+    relevance: 0,
+  };
+  const OBJECT = {
+    begin: /\{/,
+    end: /\}/,
+    contains: [VALUE_CONTAINER],
+    illegal: '\\n',
+    relevance: 0,
+  };
+  const ARRAY = {
+    begin: '\\[',
+    end: '\\]',
+    contains: [VALUE_CONTAINER],
+    illegal: '\\n',
+    relevance: 0,
+  };
+
+  const MODES = [
+    KEY,
+    {
+      className: 'meta',
+      begin: '^---\\s*$',
+      relevance: 10,
+    },
+    {
+      // multi line string
+      // Blocks start with a | or > followed by a newline
+      //
+      // Indentation of subsequent lines must be the same to
+      // be considered part of the block
+      className: 'string',
+      begin: '[\\|>]([1-9]?[+-])?[ ]*\\n( +)[^ ][^\\n]*\\n(\\2[^\\n]+\\n?)*',
+    },
+    {
+      // Ruby/Rails erb
+      begin: '<%[%=-]?',
+      end: '[%-]?%>',
+      subLanguage: 'ruby',
+      excludeBegin: true,
+      excludeEnd: true,
+      relevance: 0,
+    },
+    {
+      // named tags
+      className: 'type',
+      begin: '!\\w+!' + URI_CHARACTERS,
+    },
+    // https://yaml.org/spec/1.2/spec.html#id2784064
+    {
+      // verbatim tags
+      className: 'type',
+      begin: '!<' + URI_CHARACTERS + '>',
+    },
+    {
+      // primary tags
+      className: 'type',
+      begin: '!' + URI_CHARACTERS,
+    },
+    {
+      // secondary tags
+      className: 'type',
+      begin: '!!' + URI_CHARACTERS,
+    },
+    {
+      // fragment id &ref
+      className: 'meta',
+      begin: '&' + hljs.UNDERSCORE_IDENT_RE + '$',
+    },
+    {
+      // fragment reference *ref
+      className: 'meta',
+      begin: '\\*' + hljs.UNDERSCORE_IDENT_RE + '$',
+    },
+    {
+      // array listing
+      className: 'bullet',
+      // TODO: remove |$ hack when we have proper look-ahead support
+      begin: '-(?=[ ]|$)',
+      relevance: 0,
+    },
+    hljs.HASH_COMMENT_MODE,
+    {
+      beginKeywords: LITERALS,
+      keywords: { literal: LITERALS },
+    },
+    TIMESTAMP,
+    // numbers are any valid C-style number that
+    // sit isolated from other words
+    {
+      className: 'number',
+      begin: hljs.C_NUMBER_RE + '\\b',
+      relevance: 0,
+    },
+    OBJECT,
+    ARRAY,
+    STRING,
+  ];
+
+  const VALUE_MODES = [...MODES];
+  VALUE_MODES.pop();
+  VALUE_MODES.push(CONTAINER_STRING);
+  // @ts-ignore
+  VALUE_CONTAINER.contains = VALUE_MODES;
+
+  return {
+    name: 'YAML',
+    case_insensitive: true,
+    aliases: ['yml'],
+    contains: MODES,
+  };
+}
 
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('xml', xml);
